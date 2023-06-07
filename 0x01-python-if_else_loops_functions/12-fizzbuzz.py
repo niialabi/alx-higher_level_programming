d5877a2fe8736f4d8021a24899c195416e1b3bdf
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-07 20:57:21 +0000

    it's a wrap

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 # generate numbers separated by space
 for i in range(1, 101):
-    # check if multiple of 3 & 5 //if yes implement fizzbuzz
     if i % 3 == 0 or i % 5 == 0:
         if (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz", end=" ")
-# check if multiple of 3 //if yes implement fizz
         elif i % 3 == 0:
             print("Fizz", end=" ")
-# check if multiple of 5 // if yes implement buzz
         elif i % 5 == 0:
             print("Buzz", end=" ")
-# else print number
     else:
         print(i, end=" ")
-print()
