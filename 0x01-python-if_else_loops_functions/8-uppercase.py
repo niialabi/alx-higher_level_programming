0733159ed334d609e3782242ed6717c938b5cc59
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-07 12:59:23 +0000

    string format check fixed by introducing string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     for i in str:
         asc = int(ord(i))
         if asc >= 97 and asc <= 122:
-            asc = asc - 32
+            asc = chr(asc - 32)
         else:
-            asc = asc
-        print(chr(asc), end="")
+            asc = chr(asc)
+        print("{}".format(asc), end='')
     print()
