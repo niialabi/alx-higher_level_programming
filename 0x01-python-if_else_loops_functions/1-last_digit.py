a882799966b677fd2b174a8a0e2606ebb3b0f5f1
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-07 01:48:58 +0000

    output fix for negative numbers: corrected logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,12 @@ number = random.randint(-10000, 10000)
 absolute_last_digit = abs(number) % 10
 # changing print out format if negative / positive
 if number < 0:
-    output = "Last digit of {} is -{} and is {}"
-else:
-    output = "Last digit of {} is {} and is {}"
+    absolute_last_digit = -absolute_last_digit
+output = "Last digit of {} is {} and is {}"
 # properly formating output
 if absolute_last_digit > 5:
     print(output.format(number, absolute_last_digit, "greater than 5"))
 elif absolute_last_digit == 0:
     print(output.format(number, absolute_last_digit, 0))
-elif (absolute_last_digit > 0) and (absolute_last_digit < 6):
+elif (absolute_last_digit != 0) and (absolute_last_digit < 6):
     print(output.format(number, absolute_last_digit, "less than 6 and not 0"))
