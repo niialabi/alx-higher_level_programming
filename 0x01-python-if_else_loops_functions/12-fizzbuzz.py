1670b293d3d7a8e7ccf24434fd6f2c22bdd4fcc1
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-07 21:01:45 +0000

    function prototype added

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # generate numbers separated by space
-for i in range(1, 101):
-    if i % 3 == 0 or i % 5 == 0:
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 or i % 5 == 0:
+            if (i % 3 == 0) and (i % 5 == 0):
+                print("FizzBuzz", end=" ")
+            elif i % 3 == 0:
+                print("Fizz", end=" ")
+            elif i % 5 == 0:
+                print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
