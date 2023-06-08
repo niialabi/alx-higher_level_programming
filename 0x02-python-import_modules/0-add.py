6333ebf4b27de74989a3ad8d5c2953fde16cb547
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-08 23:01:59 +0000

    complete overhaul

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+# program imports addition function involving 2 ints and prints result of a+b
+
+
 from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
