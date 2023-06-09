f088d4f4b7bf163a17200ac811966847c72a005e
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-09 01:08:18 +0000

    1 argument condition added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-# program prints argc & argv
+# program prints argc & argv 
 
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
         print("0: arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print(f"1: {argv[1]}")
     else:
-        print(f"{(len(argv) - 1)} argument:")
+        print(f"{(len(argv) - 1)} arguments:")
         j = 1
         for i in range(len(argv)-1):
             print(f"{j}: {argv[j]}")
