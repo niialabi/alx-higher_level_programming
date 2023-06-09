5422a41b8ecafa0d100e52e7ff88ae323e051a42
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-09 01:14:21 +0000

    final fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# program prints argc & argv 
+# program prints argc & argv
 
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
-        print("0: arguments.")
+        print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
         print(f"1: {argv[1]}")
