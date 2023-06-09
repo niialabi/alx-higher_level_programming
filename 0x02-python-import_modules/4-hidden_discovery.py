7ba6ca8df5797ecb33fd01d86ffc1f20be24b515
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-09 13:20:02 +0000

    sort function removed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    hidden_list = sorted(dir(hidden_4))
+    hidden_list = dir(hidden_4)
     for i in hidden_list:
         if (i[0:2]) != "__":
             print(i)
