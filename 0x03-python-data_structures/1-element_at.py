be7d636af8ceed8ec7944b413323ccefcbf92c14
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-12 14:59:57 +0000

    none done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return ()
+        return None
     if idx >= len(my_list):
-        return ()
+        return None
     return (my_list[idx])
     
