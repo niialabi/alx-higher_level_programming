389aaa76b865e736e2717266d0b901f842181957
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-12 13:15:36 +0000

    fixed pycodestyle errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return()
+        return ()
     if idx >= len(my_list):
-        return()
-    return(my_list[idx])
+        return ()
+    return (my_list[idx])
     
