efef1233441e71fc1209d34f7d7829ed0047a2c8
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-13 05:35:25 +0000

    new line fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
-    for j in matrix:
-        for i in j:
-            print("{:d}".format(i), end=" ")
-        print()
+    if matrix != [[]]:
+        for j in matrix:
+            for i in j:
+                print("{:d}".format(i), end=" ")
+            print()
