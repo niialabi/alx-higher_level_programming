831b17e25db79727def18a114fc4086409b89ede
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-27 05:03:11 +0000

    space error fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     div = 0
     try:
-        div = a / b
+        div = a/b
     except ZeroDivisionError:
         div = None
     finally:
-        print("Inside result:{}".format(div))
+        print("Inside result: {}".format(div))
         return div
