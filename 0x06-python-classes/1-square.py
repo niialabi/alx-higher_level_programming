c6ae425d8c3ced56f2f69f812e0f261d203991f9
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-28 01:53:49 +0000

    . Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module
+create class square
+"""
+
+
+class Square:
+    """
+    class name: square
+
+    Args:
+        size of type int
+    """
+    def __init__(self, size=0):
+        """
+        privage obj(size) of square class 
+        """
+        self._Square__size = size
