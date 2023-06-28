d4b013ea12ecd6faa5b0efa713ab1df64a72585c
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-28 12:01:14 +0000

    pycode check fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,14 +34,16 @@ class Square:
         get position for c.square
         """
         return self.__position
-	
+
     @position.setter
     def position(self, value):
         """
         set position for square class
         """
-        if not isinstance(value, tuple) or len(value) != 2 or not isinstance(value[1], int) \
-                or not isinstance(value[0], int) or value[0] < 0 or value[1] < 0:
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not isinstance(value[1], int) \
+                or not isinstance(value[0], int) or \
+                value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
