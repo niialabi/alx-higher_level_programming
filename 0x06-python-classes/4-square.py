ee48a8757874a22d1db937b43bc08793350f3b1f
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-06-28 02:36:37 +0000

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Module
+create class square
+"""
+
+
+class Square:
+    """
+    class name: square
+    """
+    def __init__(self, size=0):
+        """
+        Private instance of size
+        raise value erro && type error
+
+        Args:
+        size of type int
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """
+            privage obj(size) of square class
+            """
+            self._Square__size = size
+
+    @property
+    def size(self):
+        """
+        Getter for class
+        """
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = value
+
+    def area(self):
+        """
+        returns square area
+        """
+        return self._Square__size ** 2
