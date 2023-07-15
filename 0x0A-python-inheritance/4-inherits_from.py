8bedb5002a9e9e7b48d43d6fea99ef2d7ac8705c
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-07-15 11:30:52 +0000

    only sub class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module function that returns True if the object is an
+instance of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function checks if instance of inherited class
+
+    Args:
+        Obj: Object
+        a_class: class
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
