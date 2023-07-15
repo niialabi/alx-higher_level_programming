99fe0a00fbc60066a819bbd71a89af2f3b05b9b6
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-07-15 11:51:00 +0000

    only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
         Obj: Object
         a_class: class
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return type(obj) != a_class and isinstance(obj, a_class)
