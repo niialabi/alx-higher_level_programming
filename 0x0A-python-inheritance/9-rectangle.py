b929651b9b82b13ee0f4f17b7c1368602d148c7c
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-07-15 21:58:48 +0000

    pycode error fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,7 +23,6 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
 
     def area(self):
         """
@@ -31,9 +30,9 @@ class Rectangle(BaseGeometry):
         """
         return self.__width * self.__height
 
-
     def __str__(self):
         """
         properly formated output when print obj
         """
-        return ("[" + type(self).__name__ + "] " + str(self.__width) + "/" + str(self.__height))
+        return ("[" + type(self).__name__ + "] " +
+                str(self.__width) + "/" + str(self.__height))
