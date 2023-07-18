8e777224160468a6ade5ffcc3ea349611dfd67bf
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-07-18 03:15:05 +0000

    task 1 done

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module is base of all other classes in this project
+"""
+
+
+class Base:
+    """
+    Base class for all project classes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Instantiationg Base
+
+        Args:
+            id: id(optional)
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        
