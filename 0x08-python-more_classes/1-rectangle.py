dffc8f6c0dfa0b1b4197d2e9944c230d440846c7
niialabi <taufiq.alabi@gmail.com>
2023-09-04 23:53:23 +0000

    pycode fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-        
+
     @property
     def width(self):
         """
@@ -34,7 +34,6 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be an integer")
         self.__width = value
-    
 
     @property
     def height(self):
