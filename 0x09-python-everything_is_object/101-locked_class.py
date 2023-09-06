5a693c02d27df55e9bfbc016eb0a61f9d51bb60c
niialabi <taufiq.alabi@gmail.com>
2023-09-06 04:30:32 +0000

    locked class implementation

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module containing locked class
+"""
+class LockedClass:
+    """
+    lockedclass class
+    """
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            # Allow creating the 'first_name' attribute
+            self.__dict__[name] = value
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
