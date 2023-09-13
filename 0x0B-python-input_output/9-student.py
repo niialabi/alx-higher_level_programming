fd23273cf569e0fc573515e359f9c9413853e4e0
niialabi <taufiq.alabi@gmail.com>
2023-09-13 04:00:11 +0000

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module containing student class
+"""
+
+
+class Student:
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiat self
+
+        Args:
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        method returns dic representation of student
+        """
+        return self.__dict__
