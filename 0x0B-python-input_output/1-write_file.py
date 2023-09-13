96bc2805c6c524f761b3307b964724630952b7de
niialabi <taufiq.alabi@gmail.com>
2023-09-13 03:24:24 +0000

    pycode fix 1-readfile

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,11 +7,11 @@ module contains write file function
 def write_file(filename="", text=""):
     """
     function writes string to text file
-    
+
     Args:
     filename: creates one if not found
 
     text: string to be written
     """
     with open(filename, mode="w", encoding="utf-8") as f:
-        return f.write(text);
+        return f.write(text)
