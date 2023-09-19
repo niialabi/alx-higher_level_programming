08d5a16d25a3a5fc35fbed119783ab3e8700127f
niialabi <taufiq.alabi@gmail.com>
2023-09-19 01:57:51 +0000

    task 18 dummy update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -55,3 +55,17 @@ class Base:
         if json_string == "" or json_string is None:
             return[]
         return json.loads(json_string)
+
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        Args:
+            cls: class
+            dictionary: dictionary
+        """
+        if cls.__name__ == "Rectangle":
+            allatt = cls(1, 1)
+        else:
+            allatt = cls(1)
+        cls.update(allatt, **dictionary)
+        return allatt
