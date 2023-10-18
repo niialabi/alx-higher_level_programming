b60835538ac563b95a9bf3e2b54b1e491467f2c1
Taufiq <taufiq.alabi@gmail.com>
2023-10-18 03:14:35 +0000

    syntax fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,6 +9,6 @@ VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score)
 VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score)
-VALUES (3, "Bob", 14)
+VALUES (3, "Bob", 14);
 INSERT INTO second_table (id, name, score)
 VALUES (4, "George", 8);
