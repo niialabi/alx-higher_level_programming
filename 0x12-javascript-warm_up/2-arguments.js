cc6f187015e6780555d3ffade835a80741273863
niialabi <taufiq.alabi@gmail.com>
2023-11-14 01:30:24 +0000

    args condition edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,6 @@ if (argv.length === 2) {
   console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
-} else if (argv.length > 3) {
+} else {
   console.log('Arguments found');
 }
