7f317e05b70574eb73f259d4988ee39d1334dc19
niialabi <taufiq.alabi@gmail.com>
2023-11-14 01:44:56 +0000

    syntax fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (!argv[2]) {
-  console.log('No argument');
+if (argv[2]) {
+  console.log(argv[2]);
 } else {
-  console.log(argv[2]);	
+  console.log('No argument');
 }
