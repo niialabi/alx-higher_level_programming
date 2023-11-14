e7eb8762a1634504d8830dcc857441125a1f5afa
niialabi <taufiq.alabi@gmail.com>
2023-11-14 03:11:31 +0000

    fix checker

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv.length < 3 || isNaN(Number.parseInt(argv[2],10))) {
-  console.log('Missing number of occurrences')
+if (argv.length < 3 || isNaN(Number.parseInt(argv[2], 10))) {
+  console.log('Missing number of occurrences');
 } else if (Number.parseInt(argv[2], 10) < 0) {
-
+  console.log('');
 } else {
   let i = 0;
   while (i < Number.parseInt(argv[2], 10)) {
