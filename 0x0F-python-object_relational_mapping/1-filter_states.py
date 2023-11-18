06965c4bd20f7f3aa281eeb07f8ed96babbf3c4a
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-18 15:50:06 +0000

    states that start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that lists all states with
+a name starting with N (upper N)
+ from the database hbtn_0e_0_usa:"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=db,
+        port=3306
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    db.close()
