97b920582aa714d6c796de2fa93f822152700b9b
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-19 18:41:35 +0000

    accepts user input and list cities from same city user inputed

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # connecting to DB
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+        )
+    user_input = argv[4]
+
+    # create curson object
+    cur = db.cursor()
+    query = "SELECT name FROM cities WHERE state_id = \
+        (SELECT id FROM states WHERE name = %s) ORDER BY cities.id ASC"
+    cur.execute(query, (user_input, ))
+    res = cur.fetchall()
+    i = 0
+    for cit in res:
+        for ci in cit:
+            i += 1
+            if i == 3:
+                print(ci)
+            else:
+                print(ci, end=", ")
+    cur.close()
+    db.close()
