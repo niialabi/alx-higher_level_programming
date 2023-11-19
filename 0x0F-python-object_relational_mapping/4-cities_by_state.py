ac55349a7b30fcf4e8a8405e02e757a8a143630f
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-19 16:15:19 +0000

    if name is main

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,23 +3,24 @@
 import MySQLdb
 from sys import argv
 
-# connecting to DB
-db = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    port=3306
-    )
+if __name__ == "__main__":
+    # connecting to DB
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+        )
 
-# create curson object
-cur = db.cursor()
-cur.execute("SELECT cities.id, cities.name, \
-    states.name FROM cities JOIN \
-        states ON states.id = cities.state_id \
-            ORDER BY cities.id ASC")
-res = cur.fetchall()
-for cit in res:
-    print(cit)
-cur.close()
-db.close()
+    # create curson object
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, \
+        states.name FROM cities JOIN \
+            states ON states.id = cities.state_id \
+                ORDER BY cities.id ASC")
+    res = cur.fetchall()
+    for cit in res:
+        print(cit)
+    cur.close()
+    db.close()
