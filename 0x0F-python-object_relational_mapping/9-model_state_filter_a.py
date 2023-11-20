994051f4d6e126f570aafa3594faa56608964941
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-20 04:27:50 +0000

    indentation error fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,11 +12,10 @@ if __name__ == "__main__":
         {argv[2]}@localhost:3306/{argv[3]}")
     Session = sessionmaker(bind=engine)
     session = Session()
-    output = (
-    session.query(State)
-    .filter(State.name.like('%a%'))
-    .order_by(State.id.asc())
-    .all())
+    output = (session.query(State)
+              .filter(State.name.like('%a%'))
+              .order_by(State.id.asc())
+              .all())
     for out in output:
         print(f"{out.id}: {out.name}")
     session.close()
