6604d187a5c2ffaa138347231709c62dc51e7cd4
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-20 03:35:25 +0000

    print first state if exists

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql://{argv[1]}:\
+        {argv[2]}@localhost:3306/{argv[3]}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    output = session.query(State).first()
+    if output:
+        print(f"{output.id}: {output.name}")
+    else:
+        print("Nothing")
+    session.close()
