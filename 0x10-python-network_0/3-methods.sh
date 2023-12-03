bbbea40cff510aa7e793d425f9dd33b75342552d
Taufiq Mohammed Alabi <taufiq.alabi@gmail.com>
2023-12-02 18:13:33 -0800

    variable fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+#  Bash Bash script that takes in a URL and displays all HTTP methods the server will accept.
 curl -siX OPTIONS $1 | grep "Allow:" | cut -d ' ' -f 2- 
