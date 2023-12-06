fce927447d908b01ad15f3a04e423d523311f7d9
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-06 03:17:14 +0000

    takes arg and returns member of hearder

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""script takes  URL request URL&displays value of the variable X-Request-Id"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
