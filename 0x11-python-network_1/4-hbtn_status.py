0acecf0925a8462eabe8e558bc3093e9cc9174d3
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-06 03:04:04 +0000

    output fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,4 +4,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    print(r.status_code)
+    print('Body response:')
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
