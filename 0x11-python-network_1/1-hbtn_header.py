21e83b9c91ba7c01901b5e407ba48ae07670e1d9
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-06 01:23:01 +0000

    takes an arg and returns X-Request-Id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        inf = response.info()
+        print(inf.get("X-Request-Id"))
