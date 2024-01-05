a0bcf557c5b82c845944f47cebad6958791358cb
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2024-01-05 04:59:38 +0000

    read file update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const fs = require('node:fs');
+
+const fs = require('fs');
 
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
     return;
